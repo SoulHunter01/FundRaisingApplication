c62397d844b7be787ebca4002c466d41819aae7b
Zaid Siddiqui <i180543@nu.edu.pk>
2022-01-02 21:20:27 +0500

    Implemented filter by category in RecyclerViewList

--- app/src/main/java/com/code/fundraisingapplication/RecyclerViewList.java
+++ app/src/main/java/com/code/fundraisingapplication/RecyclerViewList.java
@@ -9,7 +9,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,21 +24,26 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class RecyclerViewList extends AppCompatActivity {
+public class RecyclerViewList extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String TAG = "XA1212";
     RecyclerView rv;
     ArrayList<ImageClass> ls;
     EditText sh;
+    Spinner spinner;
+    public static String[] paths = {"All","Education","Climate Change","Medical Support"};
+
     public static String goalname;
     RecyclerViewAdapter adapter;
+    RecyclerViewAdapter adapter1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_list);
         rv=findViewById(R.id.rv);
         ls=new ArrayList<>();
-
+        spinner = (Spinner)findViewById(R.id.spinner);
         sh = (EditText) findViewById(R.id.sh);
 
         sh.setText(goalname);
@@ -55,6 +64,12 @@ public class RecyclerViewList extends AppCompatActivity {
                     filter(editable.toString());
             }
         });
+
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, paths));
+
+
+
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -106,5 +121,55 @@ public class RecyclerViewList extends AppCompatActivity {
         }
 
         adapter.filterList(filteredList);
+    }
+
+    private void filter2(String text) {
+        ArrayList<ImageClass> filteredList = new ArrayList<>();
+
+        for (ImageClass item : ls) {
+            if (item.getGoal_category().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+
+        adapter1 = new RecyclerViewAdapter(ls,RecyclerViewList.this);
+        rv.setAdapter(adapter1);
+        adapter1.filterList(filteredList);
+        adapter1.notifyDataSetChanged();
+    }
+
+    private void filter3(String text) {
+        ArrayList<ImageClass> filteredList = new ArrayList<>();
+
+        for (ImageClass item : ls) {
+                filteredList.add(item);
+        }
+
+        adapter1 = new RecyclerViewAdapter(ls,RecyclerViewList.this);
+        rv.setAdapter(adapter1);
+        adapter1.filterList(filteredList);
+        adapter1.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(RecyclerViewList.this,paths[i],Toast.LENGTH_LONG).show();
+//                if (i == 0) {
+//                    filter2("Education");
+//                } else if (i == 1) {
+//                    filter2("Climate Change");
+//                } else if (i == 3) {
+//                    filter2("Medical Support");
+//                }
+        if (paths[i] != "All") {
+            filter2(paths[i]);
+        } else {
+            filter3(paths[i]);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
